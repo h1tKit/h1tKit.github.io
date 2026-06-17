c4dcc4d10bcef2686d07d8808b9285c3e9388e66
h1tKit <2696425814@qq.com>
2026-06-17 14:51:27 +0800

    feat: add blog page, refactor projects with dynamic data, and update site content

---
+++ projects/projects.js
@@ -0,0 +1,20 @@
+var PROJECTS = [
+  {
+    "title": "Personal Website",
+    "description": "霓虹灯管 ASCII 风格个人网站，使用纯 HTML/CSS/JS 构建，部署于 GitHub Pages。",
+    "tags": ["HTML", "CSS", "JavaScript", "GitHub Pages"],
+    "link": "https://github.com/h1tKit/h1tKit.github.io"
+  },
+  {
+    "title": "LanDropMe",
+    "description": "虚拟局域网/局域网文件传输工具，双端跨平台，使用Go和Flutter开发。",
+    "tags": ["Golang", "Flutter","HTTP/2","TLS","mDNS"],
+    "link": "暂未开源"
+  },
+  {
+    "title": "SHX Music Player",
+    "description": "本地音乐播放器，使用Qt C++ & QML开发。",
+    "tags": ["Qt", "C++", "QML", "ffmpeg", "JavaScript"],
+    "link": "https://github.com/h1tKit/SHX_Music"
+  }
+];
